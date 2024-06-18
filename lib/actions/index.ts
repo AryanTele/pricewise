7c7db7521700a743280503f7202146c1b8416687
Aryan Tele <telearyan12@gmail.com>
2024-06-18 20:12:08 +0530

    connect to mongo db

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,12 +1,15 @@
 "use server";
 
 import { scrapeAmazonProduct } from "../scrapper";
+import { connectToDB } from "../scrapper/mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
   try {
+    connectToDB();
     // actual scraping
     const scrapeProduct = await scrapeAmazonProduct(productUrl);
+    if (!scrapeProduct) return;
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
