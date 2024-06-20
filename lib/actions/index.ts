84c8efc44e81bb78c42aebf624da1182eb00ba3a
Aryan Tele <telearyan12@gmail.com>
2024-06-20 13:09:14 +0530

    new lib

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,30 +1,32 @@
-"use server";
+"use server"
+
 import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
-import { scrapeAmazonProduct } from "../scrapper";
-import { connectToDB } from "../scrapper/mongoose";
+import { connectToDB } from "../mongoose";
+import { scrapeAmazonProduct } from "../scraper";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
 import { generateEmailBody, sendEmail } from "../nodemailer";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if (!productUrl) return;
+  if(!productUrl) return;
+
   try {
     connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
 
-    if (!scrapedProduct) return;
+    if(!scrapedProduct) return;
 
     let product = scrapedProduct;
 
     const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-    if (existingProduct) {
+    if(existingProduct) {
       const updatedPriceHistory: any = [
         ...existingProduct.priceHistory,
-        { price: scrapedProduct.currentPrice },
-      ];
+        { price: scrapedProduct.currentPrice }
+      ]
 
       product = {
         ...scrapedProduct,
@@ -32,7 +34,7 @@ export async function scrapeAndStoreProduct(productUrl: string) {
         lowestPrice: getLowestPrice(updatedPriceHistory),
         highestPrice: getHighestPrice(updatedPriceHistory),
         averagePrice: getAveragePrice(updatedPriceHistory),
-      };
+      }
     }
 
     const newProduct = await Product.findOneAndUpdate(
@@ -43,15 +45,17 @@ export async function scrapeAndStoreProduct(productUrl: string) {
 
     revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
-    throw new Error(`Failed to create/update product: ${error.message}`);
+    throw new Error(`Failed to create/update product: ${error.message}`)
   }
 }
 
-export async function getProductByID(productId: string) {
+export async function getProductById(productId: string) {
   try {
     connectToDB();
+
     const product = await Product.findOne({ _id: productId });
-    if (!product) return null;
+
+    if(!product) return null;
 
     return product;
   } catch (error) {
@@ -59,47 +63,54 @@ export async function getProductByID(productId: string) {
   }
 }
 
-export async function getAllProduct() {
+export async function getAllProducts() {
   try {
     connectToDB();
+
     const products = await Product.find();
+
     return products;
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function getSimilarProduct(productId: string) {
+export async function getSimilarProducts(productId: string) {
   try {
     connectToDB();
+
     const currentProduct = await Product.findById(productId);
-    if (!currentProduct) return null;
-    const similarProduct = await Product.find({
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
       _id: { $ne: productId },
     }).limit(3);
-    return similarProduct;
+
+    return similarProducts;
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function addUserEmailToProduct(
-  productId: string,
-  userEmail: string
-) {
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
   try {
     const product = await Product.findById(productId);
-    if (!product) return;
-    const userExists = product.users.some(
-      (user: User) => user.email === userEmail
-    );
 
-    if (!userExists) {
+    if(!product) return;
+
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if(!userExists) {
       product.users.push({ email: userEmail });
+
       await product.save();
 
       const emailContent = await generateEmailBody(product, "WELCOME");
+
       await sendEmail(emailContent, [userEmail]);
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 }
--- lib/models/product.model.ts
+++ lib/models/product.model.ts
@@ -1,35 +1,31 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    url: { type: String, required: true, unique: true },
-    currency: { type: String, required: true },
-    image: { type: String, required: true },
-    currentPrice: { type: Number, required: true },
-    originalPrice: { type: Number, required: true },
-    priceHistory: [
-      {
-        price: { type: Number, required: true },
-        date: { type: Date, default: Date.now },
-      },
-    ],
-    lowestPrice: { type: Number },
-    highestPrice: { type: Number },
-    averagePrice: { type: Number },
-    category: { type: String },
-    reviewsCount: { type: Number },
-    isOutOfStock: { type: Boolean },
+const productSchema = new mongoose.Schema({
+  url: { type: String, required: true, unique: true },
+  currency: { type: String, required: true },
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  currentPrice: { type: Number, required: true },
+  originalPrice: { type: Number, required: true },
+  priceHistory: [
+    { 
+      price: { type: Number, required: true },
+      date: { type: Date, default: Date.now }
+    },
+  ],
+  lowestPrice: { type: Number },
+  highestPrice: { type: Number },
+  averagePrice: { type: Number },
+  discountRate: { type: Number },
+  description: { type: String },
+  category: { type: String },
+  reviewsCount: { type: Number },
+  isOutOfStock: { type: Boolean, default: false },
+  users: [
+    {email: { type: String, required: true}}
+  ], default: [],
+}, { timestamps: true });
 
-    users: [{ email: { type: String, required: true } }],
-    default: [],
-  },
-  { timestamps: true }
-);
-
-// turn schema into model
-
-const Product =
-  mongoose.models.Product || mongoose.model("Product", productSchema);
+const Product = mongoose.models.Product || mongoose.model('Product', productSchema);
 
 export default Product;
